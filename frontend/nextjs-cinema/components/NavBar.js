6be8bb554b33134401b763cef70f96713912685b
Matthew Tzou <sonic55099@gmail.com>
2022-02-16 19:01:54 -0500

    added: mock movie

---
+++ frontend/nextjs-cinema/components/NavBar.js
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return         <div className="navbar">
+    <a id="logo">FilmMax</a>
+    <a id="searchbar"><input type="text" placeholder="Search.."></input></a>
+    <a className="navbar-button">MOVIES</a>
+    <a className="navbar-button">ABOUT US</a>
+    <a id="sign-in">Sign In</a>
+
+  </div>
+  }
---
+++ frontend/nextjs-cinema/pages/_app.js
@@ -0,0 +1,8 @@
+import '../scripts/style.css';
+import '../scripts/navbar.css';
+import '../scripts/movies.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
