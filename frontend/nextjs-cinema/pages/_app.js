6b1b49f65d6b66b0855b6fd35887b9774a2da723
MatzoMT <sonic55099@gmail.com>
2022-02-21 15:57:29 -0500

    added: page for time selection

--- frontend/nextjs-cinema/pages/_app.js
+++ frontend/nextjs-cinema/pages/_app.js
@@ -3,6 +3,7 @@ import '../scripts/navbar.css';
 import '../scripts/movies.css';
 import '../scripts/homepage.css';
 import '../scripts/adminhome.css';
+import '../scripts/booktickets.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
