e83c61f956da3961362dcce2f7a5e6ac6442fe3b
Matthew Tzou <sonic55099@gmail.com>
2022-03-24 16:57:30 -0400

    ignore this commit

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
