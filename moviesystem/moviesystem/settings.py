8a5ad205e830d243eebaaf02b0646b951923b177
Matthew Tzou <sonic55099@gmail.com>
2022-03-24 16:19:56 -0400

    junk commit

--- moviesystem/moviesystem/settings.py
+++ moviesystem/moviesystem/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'editprofile.apps.EditprofileConfig'
+    'editprofile.apps.EditprofileConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
