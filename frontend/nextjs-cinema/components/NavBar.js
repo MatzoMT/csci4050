fe32d149870dd77b448e89b337bfbf9f78e867a8
Matthew Tzou <sonic55099@gmail.com>
2022-03-27 19:18:02 -0400

    fixed: login bug

--- frontend/nextjs-cinema/components/NavBar.js
+++ frontend/nextjs-cinema/components/NavBar.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 
+
 function NavBar() {
   const router = useRouter()
 
@@ -15,51 +16,87 @@ function NavBar() {
   let signOut = async (e) => {
     e.preventDefault();
     router.push('/')
-    window.sessionStorage.setItem("email", null);
+    window.sessionStorage.removeItem("email");
   };
 
-  if (typeof window !== 'undefined' && window.sessionStorage.getItem("email") == 'null') {
-    return (<div className="navbar">
+  if (typeof window !== "undefined") {
+    // Client-side-only code
+    if (typeof window !== undefined && window.sessionStorage.getItem("email") !== null) {
+      return (<div>
+        <div className="navbar">
+          <a id="logo" href="..">FilmMax</a>
+          <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
+          <a className="navbar-button">MOVIES</a>
+          <a className="navbar-button">ABOUT US</a>
+          <a id="profile-button" onClick={showDropdown}>Profile ⋮</a>
+  
+        </div>
+        <div className="dropdown-content">
+          <div><span style={{ fontWeight: 'bold' }}>{window.sessionStorage.getItem("email")}</span></div>
+          <div><span style={{ color: '#eeeeee' }}>Edit Profile</span></div>
+          <div className="border-top"><a href="/" onClick={signOut}><span style={{ color: '#eeeeee' }}>Sign Out</span></a></div>
+        </div>
+      </div>);
+    } else {
+      return (<div className="navbar">
       <a id="logo" href="..">FilmMax</a>
       <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
       <a className="navbar-button">MOVIES</a>
       <a className="navbar-button">ABOUT US</a>
       <a id="sign-in" href="/login">Sign In</a>
-
+  
     </div>);
-  } else if (typeof window !== 'undefined') {
-    return (<div>
-      <div className="navbar">
-        <a id="logo" href="..">FilmMax</a>
-        <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
-        <a className="navbar-button">MOVIES</a>
-        <a className="navbar-button">ABOUT US</a>
-        <a id="profile-button" onClick={showDropdown}>Profile ⋮</a>
-
-      </div>
-      <div class="dropdown-content">
-          <div><span style={{fontWeight: 'bold'}}>{window.sessionStorage.getItem("email")}</span></div>
-          <div><span style={{color: '#eeeeee'}}>Edit Profile</span></div>
-          <div class="border-top"><a href="/" onClick={signOut}><span style={{color: '#eeeeee'}}>Sign Out</span></a></div>
-        </div>
-    </div>);
+    }
   } else {
-    return (<div>
-      <div className="navbar">
-        <a id="logo" href="..">FilmMax</a>
-        <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
-        <a className="navbar-button">MOVIES</a>
-        <a className="navbar-button">ABOUT US</a>
-        <a id="profile-button" onClick={showDropdown}>Profile ⋮</a>
+    return (<div className="navbar">
+    <a id="logo" href="..">FilmMax</a>
+    <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
+    <a className="navbar-button">MOVIES</a>
+    <a className="navbar-button">ABOUT US</a>
+    <a id="sign-in" href="/login">Sign In</a>
 
-      </div>
-      <div class="dropdown-content">
-          <div><span style={{color: '#eeeeee'}}>Edit Profile</span></div>
-          <div class="border-top"><a href="/" onClick={signOut}><span style={{color: '#eeeeee'}}>Sign Out</span></a></div>
-        </div>
-    </div>
-    );
+  </div>);
   }
+
+
+
+  /*
+   if (typeof window == 'undefined') {
+     return (<div className="navbar">
+       <a id="logo" href="..">FilmMax</a>
+       <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
+       <a className="navbar-button">MOVIES</a>
+       <a className="navbar-button">ABOUT US</a>
+       <a id="sign-in" href="/login">Sign In</a>
+ 
+     </div>);
+   } else if (typeof window !== 'undefined' && window.sessionStorage.getItem("email") !== 'null') {
+     return (<div>
+       <div className="navbar">
+         <a id="logo" href="..">FilmMax</a>
+         <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
+         <a className="navbar-button">MOVIES</a>
+         <a className="navbar-button">ABOUT US</a>
+         <a id="profile-button" onClick={showDropdown}>Profile ⋮</a>
+ 
+       </div>
+       <div class="dropdown-content">
+           <div><span style={{fontWeight: 'bold'}}>{window.sessionStorage.getItem("email")}</span></div>
+           <div><span style={{color: '#eeeeee'}}>Edit Profile</span></div>
+           <div class="border-top"><a href="/" onClick={signOut}><span style={{color: '#eeeeee'}}>Sign Out</span></a></div>
+         </div>
+     </div>);
+   } else {
+     return (<div className="navbar">
+     <a id="logo" href="..">FilmMax</a>
+     <a id="searchbar"><input type="text" placeholder="Search for movies.."></input></a>
+     <a className="navbar-button">MOVIES</a>
+     <a className="navbar-button">ABOUT US</a>
+     <a id="sign-in" href="/login">Sign In</a>
+ 
+   </div>
+     );
+   }*/
 }
 
 function NavBarRender(props) {
