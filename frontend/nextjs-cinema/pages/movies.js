d977ca42f1e03802fae30e544bf82240881fd630
Matthew Tzou <sonic55099@gmail.com>
2022-04-09 14:37:49 -0400

    added: rating filter

--- frontend/nextjs-cinema/pages/movies.js
+++ frontend/nextjs-cinema/pages/movies.js
@@ -9,19 +9,106 @@ import granTorino from '../images/grantorino.jpg';
 import granTorinoBanner from '../images/granTorinoBanner.jpg';
 import Footer from '../components/Footer.js';
 import React, { useState, useEffect, useCallback } from 'react';
+import theTerminal from '../images/theterminal.jpg';
+import killBillVol1 from '../images/killBill.png';
+import napoleonDynamite from '../images/napoleondynamite.jpeg'
 
+// USE RATINGS AS CLASS NAMES
+// CHANGE DIV DISPLAY DEPENDING ON WHICH CLASSES ARE SELECTED
 
 export default function Home() {
   const [titleFilter, setTitleFilter] = useState("");
 
+  const ratings = ['G', 'PG', 'PG-13', 'R'];
+  const [GRating, setGRating] = useState(false);
+  const [PGRating, setPGRating] = useState(false);
+  const [PG13Rating, setPG13Rating] = useState(false);
+  const [RRating, setRRating] = useState(false);
+  const [ratingsState, setRatingsState] = useState(
+    new Array(ratings.length)
+    // ratings
+  );
+
+  const handleRatingChange = (event, position) => {
+    alert(event.target.checked);
+    alert(position)
+
+    const updatedRatingsState = ratingsState.map((item, index) =>
+      alert(item)
+      // index === position ? !item : item
+    );
+
+    setRatingsState(updatedRatingsState);
+  };
+
+  const handleGRating = (event) => {
+    let gMovies = document.getElementsByClassName('G');
+    if (event.target.checked == false) {
+      setGRating(false);
+      /*for (let i = 0; i < gMovies.length; i++) {
+        gMovies[i].style.display = "none";
+      }*/
+    } else {
+      setGRating(true);
+      /*for (let i = 0; i < gMovies.length; i++) {
+        gMovies[i].style.display = "block";
+      }*/
+    }
+  }
+  const handlePGRating = (event) => {
+    let pgMovies = document.getElementsByClassName('PG');
+    if (event.target.checked == false) {
+      setPGRating(false);
+      /*
+      for (let i = 0; i < pgMovies.length; i++) {
+        pgMovies[i].style.display = "none";
+      }*/
+    } else {
+      setPGRating(true);
+      /*
+      for (let i = 0; i < gMovies.length; i++) {
+        pgMovies[i].style.display = "block";
+      }*/
+    }
+  }
+  const handlePG13Rating = (event) => {
+    if (event.target.checked == false) {
+      setPG13Rating(false);
+    } else {
+      setPG13Rating(true);
+    }
+  }
+  const handleRRating = (event) => {
+    let rMovies = document.getElementsByClassName('R');
+    if (event.target.checked == false) {
+      setRRating(false);
+      /*for (let i = 0; i < rMovies.length; i++) {
+        rMovies[i].style.display = "none";
+      }*/
+    } else {
+      setRRating(true);/*
+      for (let i = 0; i < rMovies.length; i++) {
+        rMovies[i].style.display = "block";
+      }*/
+    }
+  }
+
   // HARD CODE
   const currentlyShowingMovies = [
-    { "title": "Gran Torino" },
-    { "title": "The Grand Budapest Hotel" },
-    { "title": "Isle of Dogs" },
-    { "title": "Whiplash" }
+    { "title": "Gran Torino", "imageSource": granTorino, "rating": "R" },
+    { "title": "Isle of Dogs", "imageSource": isleOfDogs, "rating": "PG-13" },
+    { "title": "Whiplash", "imageSource": whiplash, "rating": "R" },
+    { "title": "The Terminal", "imageSource": theTerminal, "rating": "PG-13" },
+    { "title": "Kill Bill Vol. 1", "imageSource": killBillVol1, "rating": "R" },
+    { "title": "Napoleon Dynamite", "imageSource": napoleonDynamite, "rating": "PG"}
     // {"title": "TITLE", }
   ];
+
+  const currentlyShowingMoviesFilter = currentlyShowingMovies.filter(movie =>
+    movie["title"].toLowerCase().includes(titleFilter) && ((GRating == false && PGRating == false && PG13Rating == false && RRating == false) ||
+    (movie["rating"] == "G" && GRating == true) || (movie["rating"] == "PG" && PGRating == true) || (movie["rating"] == "PG-13" && PG13Rating == true) || (movie["rating"] == "R" && RRating == true))
+  );
+  // movie rating includes rating && rating !== false (rating is NOT undefined)
 
   const onClick = useEffect((a, b) => {
     // do something with a, b and props.x
@@ -46,13 +133,13 @@ export default function Home() {
               <h2>TITLE</h2>
               <a><input type="text" placeholder="Title..." id="filter-searchbar" onChange={(val) => setTitleFilter(val.target.value)}></input></a>
               <h2>RATING</h2>
-              <input className="filter-checkbox" type="checkbox" id="g-rating" name="g" value="G"></input>
+              <input className="filter-checkbox" type="checkbox" name="g" value="G" onChange={(e) => handleGRating(e)}></input>
               <label for="g">G</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="pg-rating" name="pg" value="PG"></input>
+              <input className="filter-checkbox" type="checkbox" name="pg" value="PG" onChange={(e) => handlePGRating(e)}></input>
               <label for="pg">PG</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="pg-13-rating" name="pg-13" value="PG-13"></input>
+              <input className="filter-checkbox" type="checkbox" name="pg-13" value="PG-13" onChange={(e) => handlePG13Rating(e)}></input>
               <label for="pg-13">PG-13</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="r-rating" name="r" value="R"></input>
+              <input className="filter-checkbox" type="checkbox" name="r" value="R" onChange={(e) => handleRRating(e)}></input>
               <label for="r">R</label><br></br>
               <h2>GENRE</h2>
               <input className="filter-checkbox" type="checkbox" id="animation" name="animation" value="Animation"></input>
@@ -65,25 +152,18 @@ export default function Home() {
               <label for="horror">Horror</label><br></br>
             </div>
             <div id="movies-column">
-             <h1>{titleFilter}</h1>
-              {
-                currentlyShowingMovies.filter(movie => movie["title"].toLowerCase().includes(titleFilter)).map((filteredMovie) =>
-                  <div>
-                    <h1>{filteredMovie["title"]}</h1>
-                  </div>
-                 )}
+              {ratingsState.map((rating, index) =>
+                <h2>{rating}</h2>
+              )}
               <h1>Currently Showing</h1>
               <div id="currently-showing-movies">
 
-                <div className="image-wrapper">
-                  <Image src={isleOfDogs} layout="responsive" sizes="50vw" />
-                  <h2>Isle of Dogs</h2>
-                </div>
-
-                <div className="image-wrapper">
-                  <Image src={granTorino} />
-                  <h2>Gran Torino</h2>
-                </div>
+                {currentlyShowingMoviesFilter.length !== 0 ? currentlyShowingMoviesFilter.map((filteredMovie) =>
+                  <div className={`image-wrapper ${filteredMovie["rating"]}`}>
+                    <Image src={filteredMovie["imageSource"]} layout="responsive" width={2} height={3} />
+                    <h4>{filteredMovie["title"]}</h4>
+                  </div>
+                ) : <h2>No movies matched your search criteria.</h2>}
 
               </div>
               <div id="coming-soon-section">
---
+++ frontend/nextjs-cinema/pages/test-api.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import NavBar from '../components/NavBar.js';
+import Script from 'next/script';
+import Image from 'next/image'
+import CurrentlyShowingMovies from '../static/currently-showing.js';
+import isleOfDogs from '../images/isleofdogs.jpg';
+import whiplash from '../images/whiplash.jpeg';
+
+// SKELETON CODE
+
+export default function TicketConfirmation() {
+  return (
+    <div className="container">
+
+        <NavBar />
+        <main >
+
+          <h1>Congrats,</h1>
+
+
+
+
+        </main>
+
+
+      <footer>
+
+      </footer>
+
+    </div>
+  )
+}
