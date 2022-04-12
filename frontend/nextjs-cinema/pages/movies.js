35fc53dcd22a9fc13288a4ea9cdd75828150c44c
Matthew Tzou <sonic55099@gmail.com>
2022-04-12 19:28:23 -0400

    added: filter by coming soon and currently showing

--- frontend/nextjs-cinema/pages/movies.js
+++ frontend/nextjs-cinema/pages/movies.js
@@ -12,7 +12,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 import theTerminal from '../images/theterminal.jpg';
 import killBillVol1 from '../images/killBill.png';
 import napoleonDynamite from '../images/napoleondynamite.jpeg';
-import MovieView from '../components/MovieView.js';
 import { Redirect } from 'react-router-dom';
 import dynamic from "next/dynamic";
 import { useRouter } from 'next/router';
@@ -29,10 +28,40 @@ export default function Home() {
   const [PGRating, setPGRating] = useState(false);
   const [PG13Rating, setPG13Rating] = useState(false);
   const [RRating, setRRating] = useState(false);
+  const [currentlyShowingFilter, setCurrentlyShowingFilter] = useState(false);
+  const [comingSoonFilter, setComingSoonFilter] = useState(false);
   const [ratingsState, setRatingsState] = useState(
     new Array(ratings.length)
     // ratings
   );
+  /*
+('1', 'COMEDY'), X
+('2', 'HORROR'), X
+('3', 'ACTION'),
+('4', 'ADVENTURE'),
+('5', 'ANIMATION'), X
+('6', 'DRAMA'), X
+('7', 'FANTASY'),
+('8', 'HISTORICAL'),
+('9', 'SCIENCE FICTION'),
+('10', 'THRILLER'),
+('11', 'WESTERN')
+*/
+  const genres = [
+    'Action',
+    'Adventure',
+    'Animation',
+    'Comedy',
+    'Drama',
+    'Fantasy',
+    'Historical',
+    'Horror',
+    'Science Fiction',
+    'Thriller',
+    'Western'
+
+  ]
+
   const router = useRouter();
 
 
@@ -91,6 +120,22 @@ export default function Home() {
       }*/
     }
   }
+  const handleCurrentlyShowing = (event) => {
+
+    if (event.target.checked == false) {
+      setCurrentlyShowingFilter(false);
+    } else {
+      setCurrentlyShowingFilter(true);
+    }
+  }
+
+  const handleComingSoon = (event) => {
+    if (event.target.checked == false) {
+      setComingSoonFilter(false);
+    } else {
+      setComingSoonFilter(true);
+    }
+  }
 
   const showMovie = (movie) => {
     //router.query.movie = movie["title"].toLowerCase().replaceAll(' ', '-');
@@ -102,24 +147,35 @@ export default function Home() {
 
   }
 
-    // HARD CODE
-    // IMPORTANT! React does not like dynamic url for Image component
-    // Solution: change each image to require('source') after getting movies
-    const currentlyShowingMovies = [
-      { "movieID": 1, "title": "Gran Torino", "imageSource": require("../images/grantorino.jpg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/RMhbr2XQblk?&autoplay=1", "description": "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, Thao Lor, a Hmong teenager who tried to steal Kowalski's prized possession: a 1972 Gran Torino.", "director": "Clint Eastwood", "genre": ""},
-      { "movieID": 2, "title": "Isle of Dogs", "imageSource": require("../images/isleofdogs.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/dt__kig8PVU?&autoplay=1", "description": "Set in Japan, Isle of Dogs follows a boy's odyssey in search of his lost dog.", "director": "Wes Anderson" },
-      { "movieID": 3, "title": "Whiplash", "imageSource": require("../images/whiplash.jpeg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/7d_jQycdQGo?&autoplay=1", "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.", "director": "Damien Chazelle"},
-      { "movieID": 4, "title": "The Terminal", "imageSource": require("../images/theterminal.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/iZqQRmhRvyg?&autoplay=1"},
-      { "movieID": 5, "title": "Kill Bill Vol. 1", "imageSource": require("../images/killBill.png"), "rating": "R", "videoLink": "https://www.youtube.com/embed/c_dNIXwrbzY?&autoplay=1"},
-      { "movieID": 6, "title": "Napoleon Dynamite", "imageSource": require("../images/napoleondynamite.jpeg"), "rating": "PG", "videoLink": "https://www.youtube.com/embed/ZHDi_AnqwN4?&autoplay=1" }
-      // {"title": "TITLE", }
+  // HARD CODE
+  // IMPORTANT! React does not like dynamic url for Image component
+  // Solution: change each image to require('source') after getting movies
+  const currentlyShowingMovies = [
+    { "movieID": 1, "title": "Gran Torino", "imageSource": require("../images/grantorino.jpg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/RMhbr2XQblk?&autoplay=1", "description": "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, Thao Lor, a Hmong teenager who tried to steal Kowalski's prized possession: a 1972 Gran Torino.", "director": "Clint Eastwood", "genre": "" },
+    { "movieID": 2, "title": "Isle of Dogs", "imageSource": require("../images/isleofdogs.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/dt__kig8PVU?&autoplay=1", "description": "Set in Japan, Isle of Dogs follows a boy's odyssey in search of his lost dog.", "director": "Wes Anderson" },
+    { "movieID": 3, "title": "Whiplash", "imageSource": require("../images/whiplash.jpeg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/7d_jQycdQGo?&autoplay=1", "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.", "director": "Damien Chazelle" },
+    { "movieID": 4, "title": "The Terminal", "imageSource": require("../images/theterminal.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/iZqQRmhRvyg?&autoplay=1" },
+
+    // {"title": "TITLE", }
   ];
+
+  const comingSoonMovies = [
+    { "movieID": 5, "title": "Kill Bill Vol. 1", "imageSource": require("../images/killBill.png"), "rating": "R", "videoLink": "https://www.youtube.com/embed/c_dNIXwrbzY?&autoplay=1" },
+    { "movieID": 6, "title": "Napoleon Dynamite", "imageSource": require("../images/napoleondynamite.jpeg"), "rating": "PG", "videoLink": "https://www.youtube.com/embed/ZHDi_AnqwN4?&autoplay=1" }
+  ]
 
   const currentlyShowingMoviesFilter = currentlyShowingMovies.filter(movie =>
     movie["title"].toLowerCase().includes(titleFilter) && ((GRating == false && PGRating == false && PG13Rating == false && RRating == false) ||
       (movie["rating"] == "G" && GRating == true) || (movie["rating"] == "PG" && PGRating == true) || (movie["rating"] == "PG-13" && PG13Rating == true) || (movie["rating"] == "R" && RRating == true))
+    && ((currentlyShowingFilter == false && comingSoonFilter == false) || (currentlyShowingFilter == true))
   );
   // movie rating includes rating && rating !== false (rating is NOT undefined)
+
+  const comingSoonMoviesFilter = comingSoonMovies.filter(movie =>
+    movie["title"].toLowerCase().includes(titleFilter) && ((GRating == false && PGRating == false && PG13Rating == false && RRating == false) ||
+      (movie["rating"] == "G" && GRating == true) || (movie["rating"] == "PG" && PGRating == true) || (movie["rating"] == "PG-13" && PG13Rating == true) || (movie["rating"] == "R" && RRating == true))
+    && ((comingSoonFilter == false && currentlyShowingFilter == false) || (comingSoonFilter == true))
+  );
 
   const onClick = useEffect((a, b) => {
     // do something with a, b and props.x
@@ -144,6 +200,11 @@ export default function Home() {
               <h1>Filter By</h1>
               <h2>TITLE</h2>
               <a><input type="text" placeholder="Title..." id="filter-searchbar" onChange={(val) => setTitleFilter(val.target.value)}></input></a>
+              <h2>SCREENING</h2>
+              <input className="filter-checkbox" type="checkbox" name="currently-showing" value="currently-showing" onChange={(e) => handleCurrentlyShowing(e)}></input>
+              <label for="currently-showing">Currently Showing</label><br></br>
+              <input className="filter-checkbox" type="checkbox" name="coming-soon" value="coming-soon" onChange={(e) => handleComingSoon(e)}></input>
+              <label for="coming-soon">Coming Soon</label><br></br>
               <h2>RATING</h2>
               <input className="filter-checkbox" type="checkbox" name="g" value="G" onChange={(e) => handleGRating(e)}></input>
               <label for="g">G</label><br></br>
@@ -154,26 +215,25 @@ export default function Home() {
               <input className="filter-checkbox" type="checkbox" name="r" value="R" onChange={(e) => handleRRating(e)}></input>
               <label for="r">R</label><br></br>
               <h2>GENRE</h2>
-              <input className="filter-checkbox" type="checkbox" id="animation" name="animation" value="Animation"></input>
-              <label for="animation">Animation</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="comedy" name="comedy" value="Comedy"></input>
-              <label for="comedy">Comedy</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="drama" name="drama" value="Drama"></input>
-              <label for="drama">Drama</label><br></br>
-              <input className="filter-checkbox" type="checkbox" id="horror" name="horror" value="Horror"></input>
-              <label for="horror">Horror</label><br></br>
+
+              {
+                genres.map((genre) =>
+                  <div>
+                    <input className="filter-checkbox" type="checkbox" id={genre.toLowerCase()} name={genre.toLowerCase()} value={genre.toLowerCase()}></input>
+                    <label for={genre.toLowerCase()}>{genre}</label><br></br>
+
+                  </div>
+                )
+              }
             </div>
             <div id="movies-column">
-              {ratingsState.map((rating, index) =>
-                <h2>{rating}</h2>
-              )}
-              <h1>Currently Showing</h1>
+              {comingSoonFilter == false || (comingSoonFilter == true && currentlyShowingFilter == true) ? <h1>Currently Showing</h1> : <h1>Adjust filter to view currently showing movies.</h1>}
               <div id="currently-showing-movies">
 
                 {currentlyShowingMoviesFilter.length !== 0 ? currentlyShowingMoviesFilter.map((filteredMovie) =>
                   <div className={`image-wrapper ${filteredMovie["rating"]}`} onClick={() => showMovie(filteredMovie)}>
 
-                    <Image src={filteredMovie["imageSource"]} height={300} width={200}/>
+                    <Image src={filteredMovie["imageSource"]} height={300} width={200} />
                     <h4>{filteredMovie["title"]}</h4>
 
                     <p class="image-wrapper-rating">{filteredMovie["rating"]}</p>
@@ -184,12 +244,17 @@ export default function Home() {
               <div id="coming-soon-section">
 
 
-                <h1>Coming Soon</h1>
-                <div id="coming-soon-movies">
-                  <div className="image-wrapper">
-                    <Image src={whiplash} />
-                    <h2>Whiplash</h2>
-                  </div>
+              {currentlyShowingFilter == false || (currentlyShowingFilter == true && comingSoonFilter == true) ? <h1>Coming Soon</h1> : <h1>Adjust filter to view coming soon movies.</h1>}
+                <div id="currently-showing-movies">
+                  {comingSoonMoviesFilter.length !== 0 ? comingSoonMoviesFilter.map((filteredMovie) =>
+                    <div className={`image-wrapper ${filteredMovie["rating"]}`} onClick={() => showMovie(filteredMovie)}>
+
+                      <Image src={filteredMovie["imageSource"]} height={300} width={200} />
+                      <h4>{filteredMovie["title"]}</h4>
+
+                      <p class="image-wrapper-rating">{filteredMovie["rating"]}</p>
+                    </div>
+                  ) : <h2>No movies matched your search criteria.</h2>}
                 </div>
               </div>
             </div>
