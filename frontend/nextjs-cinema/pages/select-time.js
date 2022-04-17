00bf2716ac8bbcdbd817cb0649b73d5b765eebb9
Matthew Tzou <sonic55099@gmail.com>
2022-04-17 17:01:21 -0400

    updated: select-time.js functionality

--- frontend/nextjs-cinema/pages/select-time.js
+++ frontend/nextjs-cinema/pages/select-time.js
@@ -1,38 +1,107 @@
 import NavBar from '../components/NavBar.js';
 import isleOfDogs from '../images/isleofdogs.jpg';
 import Image from 'next/image';
+import { useState, useEffect, useSearchParams } from 'react';
+import { useRouter } from 'next/router';
+
 
 
 
 // This is a hardcoded example
 // In the future, pass movie metadata as props?
 export default function SelectTime() {
+    const [movie, setMovie] = useState({});
+    const [showTimes, setShowTimes] = useState([]);
+    const router = useRouter();
+
+
+    const currentlyShowingMovies = [
+        { "movieID": 1, "title": "Gran Torino", "imageSource": require("../images/grantorino.jpg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/RMhbr2XQblk?&autoplay=1", "description": "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, Thao Lor, a Hmong teenager who tried to steal Kowalski's prized possession: a 1972 Gran Torino.", "director": "Clint Eastwood" },
+        { "movieID": 2, "title": "Isle of Dogs", "imageSource": require("../images/isleofdogs.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/dt__kig8PVU?&autoplay=1", "description": "Set in Japan, Isle of Dogs follows a boy's odyssey in search of his lost dog.", "director": "Wes Anderson" },
+        { "movieID": 3, "title": "Whiplash", "imageSource": require("../images/whiplash.jpeg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/7d_jQycdQGo?&autoplay=1", "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.", "director": "Damien Chazelle" },
+        { "movieID": 4, "title": "The Terminal", "imageSource": require("../images/theterminal.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/iZqQRmhRvyg?&autoplay=1" },
+        { "movieID": 5, "title": "Kill Bill Vol. 1", "imageSource": require("../images/killBill.png"), "rating": "R", "videoLink": "https://www.youtube.com/embed/c_dNIXwrbzY?&autoplay=1" },
+        { "movieID": 6, "title": "Napoleon Dynamite", "imageSource": require("../images/napoleondynamite.jpeg"), "rating": "PG", "videoLink": "https://www.youtube.com/embed/ZHDi_AnqwN4?&autoplay=1" }
+        // {"title": "TITLE", }
+    ];
+
+    const movieShows = [
+        { "movieID": 1, "roomID": 1, "show_date": "04/19/22", "show_time": "15:00" },
+        { "movieID": 1, "roomID": 1, "show_date": "04/19/22", "show_time": "18:00" },
+        { "movieID": 1, "roomID": 1, "show_date": "04/21/22", "show_time": "19:00" },
+
+
+    ]
+
+    //const [alreadyShownDates, setAlreadyShownDates] = useState([]);
+    let alreadyShownDates = []
+
+    useEffect(() => {
+        for (let i = 0; i < currentlyShowingMovies.length; i++) {
+            if (currentlyShowingMovies[i]["movieID"] == router.query.movieID) {
+                setMovie(currentlyShowingMovies[i]);
+            }
+        }
+        for (let i = 0; i < movieShows.length; i++) {
+            if (movieShows[i]["movieID"] == router.query.movieID) {
+                setShowTimes(showTimes => [...showTimes, movieShows[i]]);
+            }
+        }
+    }, []);
+
+    const renderTimes = () => {
+        let result = [];
+        for (let i = 0; i < movieShows.length; i++) {
+            if (alreadyShownDates.indexOf(movieShows[i]["show_date"]) > -1) {
+                continue;
+            } else {
+                let resultDate = [];
+                for (let j = i; j < movieShows.length; j++) {
+                    if (movieShows[j]["movieID"] == router.query.movieID && movieShows[j]["show_date"] == movieShows[i]["show_date"]) {
+                        resultDate.push(<h3>{movieShows[j]["show_time"]}</h3>);
+
+                    }
+                }
+                result.push(<div className="book-movie-date">
+                    <h2>{movieShows[i]["show_date"]}</h2>
+                    <div className="book-movie-date-time">
+
+                        {resultDate}
+                    </div>
+                </div>)
+                alreadyShownDates.push(movieShows[i]["show_date"]);
+            }
+        }
+        return result;
+    }
+    /*
+    iterate through each showtime
+    if date is not in alreadyShownDates, show dates
+    after map iteration, add each date to alreadyShownDates
+    */
     return <div>
         <NavBar />
         <div class="book-movie-poster">
-            <Image src={isleOfDogs} />
+            {movie["imageSource"] !== undefined && <Image src={movie["imageSource"]} height={300} width={200} />}
         </div>
 
         <div className="book-movie-info">
-            <h1 className="book-movie-title">Isle of Dogs</h1>
+            <h1 className="book-movie-title">{movie["title"]}</h1>
             <h2 className="book-movie-available-times">Available Times</h2>
-            <div className="book-movie-date">
-                <h2>Wednesday, February 23</h2>
-                <div className="book-movie-date-time">
-                    <h3>4:30</h3>
-                    <h3>6:45</h3>
-                    <h3>9:00</h3>
+            {renderTimes()}
+            {/*showTimes.map((showDate) =
+            
+                <div className="book-movie-date">
+                    <h2>{showDate["show_date"]}</h2>
+                    <div className="book-movie-date-time">
+                        {showTimes.map((showTime) => 
+                        <h3>{showTime["show_date"] == showDate["show_date"] && showTime["show_time"]}</h3>
+                        )}
+                    </div>
                 </div>
-            </div>
-            <div className="book-movie-date">
-                <h2>Thursday, February 24</h2>
-                <div className="book-movie-date-time">
-                    <h3>2:15</h3>
-                    <h3>4:30</h3>
-                    <h3>6:45</h3>
-                    <h3>9:00</h3>
-                </div>
-            </div>
+                        
+                        )*/}
+
         </div>
 
 
--- frontend/nextjs-cinema/pages/view-movie.js
+++ frontend/nextjs-cinema/pages/view-movie.js
@@ -9,16 +9,16 @@ export default function Home(movieName) {
     const [movie, setMovie] = useState({});
     const { query } = useRouter();
     const router = useRouter();
-    
+
     // HARD CODE
     // IMPORTANT! React does not like dynamic url for Image component
     // Solution: change each image to require('source') after getting movies
     const currentlyShowingMovies = [
-        { "movieID": 1, "title": "Gran Torino", "imageSource": require("../images/grantorino.jpg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/RMhbr2XQblk?&autoplay=1", "description": "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, Thao Lor, a Hmong teenager who tried to steal Kowalski's prized possession: a 1972 Gran Torino.", "director": "Clint Eastwood"},
+        { "movieID": 1, "title": "Gran Torino", "imageSource": require("../images/grantorino.jpg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/RMhbr2XQblk?&autoplay=1", "description": "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, Thao Lor, a Hmong teenager who tried to steal Kowalski's prized possession: a 1972 Gran Torino.", "director": "Clint Eastwood" },
         { "movieID": 2, "title": "Isle of Dogs", "imageSource": require("../images/isleofdogs.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/dt__kig8PVU?&autoplay=1", "description": "Set in Japan, Isle of Dogs follows a boy's odyssey in search of his lost dog.", "director": "Wes Anderson" },
-        { "movieID": 3, "title": "Whiplash", "imageSource": require("../images/whiplash.jpeg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/7d_jQycdQGo?&autoplay=1", "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.", "director": "Damien Chazelle"},
-        { "movieID": 4, "title": "The Terminal", "imageSource": require("../images/theterminal.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/iZqQRmhRvyg?&autoplay=1"},
-        { "movieID": 5, "title": "Kill Bill Vol. 1", "imageSource": require("../images/killBill.png"), "rating": "R", "videoLink": "https://www.youtube.com/embed/c_dNIXwrbzY?&autoplay=1"},
+        { "movieID": 3, "title": "Whiplash", "imageSource": require("../images/whiplash.jpeg"), "rating": "R", "videoLink": "https://www.youtube.com/embed/7d_jQycdQGo?&autoplay=1", "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.", "director": "Damien Chazelle" },
+        { "movieID": 4, "title": "The Terminal", "imageSource": require("../images/theterminal.jpg"), "rating": "PG-13", "videoLink": "https://www.youtube.com/embed/iZqQRmhRvyg?&autoplay=1" },
+        { "movieID": 5, "title": "Kill Bill Vol. 1", "imageSource": require("../images/killBill.png"), "rating": "R", "videoLink": "https://www.youtube.com/embed/c_dNIXwrbzY?&autoplay=1" },
         { "movieID": 6, "title": "Napoleon Dynamite", "imageSource": require("../images/napoleondynamite.jpeg"), "rating": "PG", "videoLink": "https://www.youtube.com/embed/ZHDi_AnqwN4?&autoplay=1" }
         // {"title": "TITLE", }
     ];
@@ -29,8 +29,14 @@ export default function Home(movieName) {
                 setMovie(currentlyShowingMovies[i]);
             }
         }
-    }, []);
+    });
 
+    const bookMovie = () => {
+        router.push({
+            pathname: '/select-time',
+            query: { "movieID": router.query.movieID },
+        })
+    }
 
 
     return <div>
@@ -40,7 +46,7 @@ export default function Home(movieName) {
         </div>
         <div className="movie-info">
             <h1 className="movie-title">{movie["title"]}</h1>
-            <button className="book-button" type="button"><a href="http://localhost:3000/select-time" className="book-tickets-text">BOOK TICKETS</a></button>
+            <button className="book-button" type="button" onClick={() => bookMovie() }><a className="book-tickets-text">BOOK TICKETS</a></button>
             <h2 className="movie-description">{movie["description"]}</h2>
             <p className="movie-director">Director: Wes Anderson</p>
             <p className="movie-genre">Genre: Comedy, Adventure</p>
